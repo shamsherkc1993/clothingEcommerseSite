aafc0becb9ef9b6d713bdc469380d1e54f0d220a
shamsher <shamsher.kc.c4@gmail.com>
2025-07-09 13:27:53 +0900

    single page done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Productlisting from "./product-listing/Productlisting";
+import SingleProduct from "./pages/singleproduct/SingleProduct";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/product-listing" element={<Productlisting />} />
+        <Route path="/product-detail-single" element={<SingleProduct />} />
       </Routes>
     </>
   );
