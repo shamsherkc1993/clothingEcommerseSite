398cc06dcb606fc53b3a8739e64e633bc6500917
shamsher <shamsher.kc.c4@gmail.com>
2025-07-09 12:58:26 +0900

    product-listing page done

--- src/product-listing/Productlisting.jsx
+++ src/product-listing/Productlisting.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Breadcumb from "./Breadcumb";
+import Mainproductcontent from "./Mainproductcontent";
 
 const Productlisting = () => {
   return (
     <>
-    <Header/>
-    
-    <Footer/>
-      
+      <Header />
+      <div className="page-container">
+        <Breadcumb />
+        <Mainproductcontent />
+      </div>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Productlisting
+export default Productlisting;
