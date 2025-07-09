30bb50be37b9e80e96b9b07ca828a6f9c816fab5
shamsher <shamsher.kc.c4@gmail.com>
2025-07-09 13:55:10 +0900

    contact and about done

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,21 @@ import Productlisting from "./product-listing/Productlisting";
 import SingleProduct from "./pages/singleproduct/SingleProduct";
 import Cart from "./pages/cart/Cart";
 import Checkout from "./pages/checkout/Checkout";
+import About from "./pages/About/About";
+import Contact from "./pages/contact/Contact";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/product-listing" element={<Productlisting />} />
         <Route path="/product-detail-single" element={<SingleProduct />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/checkout" element={<Checkout />} />
-      </Routes>
+       </Routes>
     </>
   );
 }
--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -6,7 +6,66 @@ const About = () => {
   return (
     <div>
       <Header />
-      <p>about</p>
+      <div className="page-contaiter">
+        {/*Breadcrumb*/}
+        <section className="breadcrumb">
+          <div
+            className="background-image"
+            data-background="img/bg_img/bg_001.jpg"
+            data-bg-posx="center"
+            data-bg-posy="center"
+            data-bg-overlay={4}
+          />
+          <div className="breadcrumb-content">
+            <div className="container">
+              <div className="row">
+                <div className="col-12">
+                  <h1 className="breadcrumb-title">About Us</h1>
+                  <nav className="breadcrumb-link">
+                    <span>
+                      <a href="home.html">Home</a>
+                    </span>
+                    <span>About Us</span>
+                  </nav>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        {/*Breadcrumb*/}
+        {/*About*/}
+        <section className="sec-padding">
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-12 col-md-7 col-lg-6 d-flex mb-sm-5">
+                <div className="align-self-center">
+                  <h2>Welcome to Mazaar Store!</h2>
+                  <p className="large">
+                    Mazaar Shop is a premium HTML template designed and
+                    develoved from the ground up with the sole purpose of
+                    helping you create an astonishing, the beautiful and user
+                    friendly website that will boost your product’s sales.
+                  </p>
+                  <p className="color--dark text--bold">
+                    The theme design package provides a complete Ecommerce theme
+                    set for your online store according to your desired theme.
+                    This includes all Ecommerce themes that are required for
+                    your online store's successful implementation.
+                  </p>
+                  <a href="#" className="btn btn--primary space-t--1">
+                    Shop Now
+                  </a>
+                </div>
+              </div>
+              <div className="col-sm-12 col-md-5 col-lg-6">
+                <img src="img/categories/collection_006.jpg" alt />
+              </div>
+            </div>
+          </div>
+        </section>
+        {/*End About*/}
+      </div>
+
       <Footer />
     </div>
   );
